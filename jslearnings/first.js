1791c0b192a84076a40c9a7c0ffba05a58ed268f
swathini <swathini0999@gmail.com>
2026-09-17 19:11:49 +0530

    revised

--- jslearnings/first.js
+++ jslearnings/first.js
@@ -1,2 +1,3 @@
-let name = "John Doe";
-console.log("Hello, " + name + "! Welcome to JavaScript learning.");
+let nam = "John Doe";
+console.log("Hello, " + nam + "! Welcome to JavaScript learning.");
+document.getElementById("output").innerHTML = "Hello World";
